0bc45a0865d1737165b0cd510865354710b1bc20
ravira03 <ravivarmara22@gmail.com>
2021-02-21 09:40:08 +0530

    to-do-api folder

---
+++ to-do-api/models/note.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'user' },
+    createdOn: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('note', noteSchema);
